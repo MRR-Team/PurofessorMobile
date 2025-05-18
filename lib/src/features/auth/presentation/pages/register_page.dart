1576937339c38bb01c618d4d40efe08ebacdaecc
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-05-18 22:57:16 +0200

    feat: Add register page
    This commit introduces the register page and its associated form widget.
    
    - Added `RegisterPage` for user registration.
    - Created `RegisterForm` widget for handling registration inputs.
    - Implemented register functionality in `AuthController`.

---
+++ lib/src/features/auth/presentation/pages/register_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:purofessor_mobile/src/shared/presentation/widgets/app_background.dart';
+import 'package:purofessor_mobile/src/shared/presentation/widgets/button.dart';
+import 'package:purofessor_mobile/src/core/constants/app_constatns.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppBackground(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    AppConstants.appName,
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 12),
+
+                  Text(
+                    'Zarejestruj się',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40),
+
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: const Icon(Icons.email_outlined),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Hasło',
+                      prefixIcon: const Icon(Icons.lock_outline),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+
+                  Button(
+                    label: 'Zarejestruj',
+                    fullWidth: true,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/auth/presentation/widgets/register_form.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:purofessor_mobile/src/shared/presentation/widgets/button.dart';
+import 'package:purofessor_mobile/src/features/auth/presentation/controllers/auth_controller.dart';
+
+class RegisterForm extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final TextEditingController emailController;
+  final TextEditingController nameController;
+  final TextEditingController passwordController;
+  final TextEditingController confirmPasswordController;
+
+  const RegisterForm({
+    super.key,
+    required this.formKey,
+    required this.emailController,
+    required this.nameController,
+    required this.passwordController,
+    required this.confirmPasswordController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final authController = Provider.of<AuthController>(context);
+
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              labelStyle: TextStyle(color: Colors.white70),
+              prefixIcon: Icon(Icons.email_outlined),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Podaj email';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: nameController,
+            keyboardType: TextInputType.name,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: 'Nazwa',
+              labelStyle: TextStyle(color: Colors.white70),
+              prefixIcon: Icon(Icons.email_outlined),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Podaj nazwe';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: passwordController,
+            obscureText: true,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: 'Hasło',
+              labelStyle: TextStyle(color: Colors.white70),
+              prefixIcon: Icon(Icons.lock_outline),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Podaj hasło';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: confirmPasswordController,
+            obscureText: true,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: 'Potwierdź Hasło',
+              labelStyle: TextStyle(color: Colors.white70),
+              prefixIcon: Icon(Icons.lock_outline),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Powórz hasło';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+
+          Button(
+            label: 'Zarejestruj się',
+            isLoading: authController.isLoading,
+            fullWidth: true,
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                authController.register(
+                  context,
+                  emailController.text,
+                  nameController.text,
+                  passwordController.text,
+                  confirmPasswordController.text,
+                );
+              }
+            },
+          ),
+
+          const SizedBox(height: 16),
+
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/guest');
+            },
+            child: const Text(
+              'Wejdź jako gość',
+              style: TextStyle(color: Colors.white70),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
