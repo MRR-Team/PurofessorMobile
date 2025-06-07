f3ee31f09f7947a080a8642935c5d6b7d0a247ee
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-06-07 12:25:50 +0200

    Refactor: Remove unused getCurrentUser and use User model
    - Removed the `getCurrentUser` method from `AuthRepository` and its implementation in `AuthRepositoryImpl` as it was unused.
    - Introduced a `User` model to represent user data.
    - Updated `AuthDataSource` to parse and store `User` objects instead of raw JSON.
    - Updated `AuthController` to use the `User` model for user data and removed the direct use of SharedPreferences for user loading.
    - Adjusted `HomePage` to conditionally display login/logout buttons based on `isLoggedIn` status and removed the direct display of username.

--- lib/src/features/auth/domain/repositories/auth_repository.dart
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -1,6 +1,5 @@
 abstract class AuthRepository {
   Future<void> login(String email, String password);
-  Future<Map<String, dynamic>?> getCurrentUser();
   Future<void> register(
     String email,
     String name,
